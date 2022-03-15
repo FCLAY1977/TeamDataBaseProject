3d3a3b34d31f4fb8fbe6ea3d8227eb4d3d2967d8
Fred Clay <fred.clay1977@gmail.com>
2022-03-15 18:41:43 -0400

    my first commit

---
+++ Team Database Project/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CodeLouisvilleLibrary;
+using System.Linq;
+using System.Text;
+
+
+namespace TeamDatabase
+{
+    
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position;
+        public decimal Height;
+        public int Weight;
+        public int TeamNumber;
+        
+        public AddPLayer(StringBuilder)
+        {
+            Console.WriteLine("Please enter your player name.");
+        }
+
+
+
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args);
+        {
+            Player player = new Player();
+            
+        
+        }
+
+    }
+
+
+
+
+
+
+
